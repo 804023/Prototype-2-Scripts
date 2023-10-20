4925d1ecfc7f499ebe3e7598ea1b90080d88f505
Andrew <804023@sylvaniastudents.org>
2023-10-20 10:09:28 -0400

    2.2.2

--- MoveForward.cs
+++ MoveForward.cs
@@ -9,10 +9,10 @@ public class MoveForward : MonoBehaviour
     {
         
     }
-
+    public float speed = 40.0f;
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
     }
 }
--- PlayerController.cs
+++ PlayerController.cs
@@ -12,6 +12,8 @@ public class PlayerController : MonoBehaviour
     public float horizontalInput;
     public float speed = 10.0f;
     public float xRange = 10;
+
+    public GameObject projectilePrefab;
     // Update is called once per frame
     void Update()
     {
