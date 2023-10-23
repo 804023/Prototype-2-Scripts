0942ceae79575f7bb705f425f68ecbdd7565b9e7
Andrew <804023@sylvaniastudents.org>
2023-10-23 09:58:46 -0400

    2.2.7.2

--- DestroyOutOfBounds.cs
+++ DestroyOutOfBounds.cs
@@ -20,7 +20,7 @@ public class DestroyOutOfBounds : MonoBehaviour
         }
         else if (transform.position.z < lowerBound)
         {
-            DestroyOutOfBounds(gameObject);
+            Destroy(gameObject);
         }
     }
 }
